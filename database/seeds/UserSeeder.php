01342434f414d39a5a0985ce9c29e6b8f04db56f
thomkok20 <tkok@roc-dev.com>
2019-01-15 18:19:19 +0100

    update

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Thom Kok',
+            'email' => 'thomkok13@hotmail.com',
+            'password' => bcrypt('testen13'),
+            'cover_image' => 'thomkok21.png',
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
+}
